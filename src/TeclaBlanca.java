4cf833b204fceaa899660b63d82fe398db9e0a1b
DOD-SMR <ortegadelgadod@gmail.com>
2025-04-06 20:23:44 +0200

    TeclaBlanca Terminada

--- src/TeclaBlanca.java
+++ src/TeclaBlanca.java
@@ -1,7 +1,24 @@
+import java.awt.*;
+
 public abstract class TeclaBlanca extends Tecla{
+    public static final int ALTURA = 100;
+    public static final int ANCHURA = 25;
     TeclaBlanca(int numero){
         super(numero);
     }
+    @Override
+    public Color getColorNoPulsado(){
+        return new Color(255,255,255);
+    }
+    @Override
+    public int getAnchura(){
+        return TeclaBlanca.ANCHURA;
+    }
+    @Override
+    public int getAltura(){
+        return TeclaBlanca.ALTURA;
+    }
+
 
 
 }
