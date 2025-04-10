f0666c42d82af59da94bdeffbcf94f31104d2a3a
Raidel Martinez <raidelmfernandez@gmail.com>
2025-04-10 17:08:01 +0200

    Empezado el programa hay que programar las clases heredadas de piano en multipiano

--- src/MultiPiano.java
+++ src/MultiPiano.java
@@ -1,9 +1,40 @@
+import java.awt.*;
 import java.util.List;
 
-public class MultiPiano {
+public class MultiPiano extends Piano{
     private List<PianoSencillo> piano;
 
     public MultiPiano(int teclainicial,int teclaFinal){
 
+    }
+
+    @Override
+    public Tecla getTecla(int canal, int nota) {
+        return null;
+    }
+
+    @Override
+    public void setPosicion(int x, int y) {
+
+    }
+
+    @Override
+    public void setGraphics(Graphics g) {
+
+    }
+
+    @Override
+    public void dibujar() {
+
+    }
+
+    @Override
+    public int getAnchura() {
+        return 0;
+    }
+
+    @Override
+    public int getAltura() {
+        return 0;
     }
 }
