e080d230d81902040813ac3b6d1f9f600121ffb0
DOD-SMR <ortegadelgadod@gmail.com>
2025-04-10 19:25:51 +0200

    Intento de recuperacion de datos

--- src/TeclaNegraTest.java
+++ src/TeclaNegraTest.java
@@ -7,24 +7,17 @@ public class TeclaNegraTest {
 
     @Test
     public void test1(){
-
         TeclaNegra teclaNegra = new TeclaNegra(61);
         teclaNegra.setColorPulsado(Color.BLUE);
         teclaNegra.pulsar();
         assertEquals(Color.BLUE,teclaNegra.getColor());
-
     }
 
     @Test
-
     public void test2(){
         TeclaNegra teclaNegra = new TeclaNegra(61);
-
         ElementoVisualTester elementoVisualTester= new ElementoVisualTester();
         elementoVisualTester.setElementoVisual(teclaNegra);
-
         elementoVisualTester.hacerPrueba();
-
     }
-  
 }
